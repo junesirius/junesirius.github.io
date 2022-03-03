99ec34a2593c9b65747bc6a44dd4de316fca8ca1
junesirius <wojiaoziyou@gmail.com>
2022-03-02 23:00:44 -0500

    Add pre-commit hooks

---
+++ .pre-commit.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# encoding=utf-8
+from pathlib import Path
+import subprocess
+import sys
+from termcolor import colored
+
+# print Chinese characters in terminal
+sys.stdout.reconfigure(encoding="utf-8")
+
+
+def check_and_auto_update():
+    # Check all files in the staging-area
+    file_list = (
+        subprocess.Popen(
+            "git diff --name-only --staged",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+        .communicate()[0]
+        .decode("utf-8")
+        .split("\n")[:-1]
+    )
+
+    file_changed = []
+
+    for file in file_list:
+        if file.endswith(".md") and Path(file).exists():
+            file = Path(file)
+            print(f"Checking {colored(file, 'yellow')} ...", end="")
+            with file.open("r", encoding="utf-8") as f:
+                content = f.read()
+            if "\n" * 4 in content:
+                file_changed.append(file)
+                print(
+                    colored(
+                        "\n\tNote: Empty paragraph found!"
+                        "\n\tWarning: Starting to edit file content...",
+                        "red",
+                    ),
+                    end="",
+                )
+                content = content.replace("\n" * 4, "\n\n<br>\n\n")
+                with file.open("w", encoding="utf-8") as f:
+                    f.write(content)
+            print(colored("Done.", "green"))
+
+    if file_changed:
+        print("\nChanged files: ")
+        for file in file_changed:
+            print(colored(f"\t{file}", "red"))
+        print("\nPlease check file difference and re-commit.\n\n")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    check_and_auto_update()
---
+++ pre-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#!/usr/bin/env bash
+
+INSTALL_PYTHON="G:\Anaconda3\envs\py38\python.exe"
+PYTHON_SCRIPT=".pre-commit.py"
+
+if [ -x "$INSTALL_PYTHON" ]; then
+    if [ -f "$PYTHON_SCRIPT" ]; then
+        python $PYTHON_SCRIPT
+    else
+        echo "$PYTHON_SCRIPT not found. Skipping pre-commit checking."
+    fi
+else
+    echo "'pre-commit' not found. Skipping pre-commit checking."
+fi
+
+if [ $? != 0 ]; then
+    echo "git status:"
+    git status
+    exit 1
+fi
